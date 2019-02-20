31c3f7107575e2718554a6bff671f81b4d6967d5
Nicolas Charpin <xleto@hotmail.com>
2019-02-20 12:44:52 +0100

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -7,10 +7,16 @@ socket.on("disconnect", function() {
 	console.log("Disconnected from server");
 });
 socket.on("newMessage", function(message) {
-	console.log("New message received", message);
+	const li = $("<li></li>").html(`${message.from}: ${message.text}`);
+	$("#messages").append(li);
 });
 
-// socket.emit("createMessage", {
-// 	to: "Oliv",
-// 	text: "You're kro fort !"
-// });
+$("#messageForm").on("submit", function(event){
+	event.preventDefault();
+	socket.emit("createMessage", {
+		from: "Nico",
+		text: $("#message").val()
+	}, function(data) {
+		console.log("got it ! : ", data);
+	});
+});
